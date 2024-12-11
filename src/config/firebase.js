858735ecfc449e744ce3aa4f0f00e59dad276ac4
Sheila Fernandez Gonzalez <126667454+sheifg@users.noreply.github.com>
2024-12-11 07:20:15 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,18 +7,4 @@ export const firebaseConfig = {
   appId: import.meta.env.VITE_appId,
 };
 
-console.log(firebaseConfig);
 
-//The credentials( apiKey,....) are taken from firebase and we will use in .env to encrypt them
-// const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_AUTH_DOMAIN",
-//   projectId: "YOUR_PROJECT_ID",
-//   storageBucket: "YOUR_STORAGE_BUCKET",
-//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//   appId: "YOUR_APP_ID",
-// };
-
-// Create react app the syntax is different
-// If server is created with create-react-app
-//  apiKey: process.env.REACT_APP_apiKey,
